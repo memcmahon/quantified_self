d646b8d56bdf3d9d22ceb2edfe03b038668232f3
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-30 17:03:48 -0600

    adds food#destroy

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -6,4 +6,10 @@ class Api::V1::FoodsController < ApplicationController
   def show
     render json: Food.find(params[:id])
   end
+
+  def destroy
+    Food.destroy(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render status: 404, json: { message: "Food Not Found" }
+  end
 end
