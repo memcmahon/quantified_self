212d0f1924423d9a2c22ca1d39939fe66987e72b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-30 16:39:40 -0600

    Adds single meal route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :foods, only: [:index, :show]
       resources :meals, only: [:index]
+      get '/meals/:id/foods', to: 'meals#show'
     end
   end
 end
